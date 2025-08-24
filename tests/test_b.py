66f2e184f2b68478c5a6f0bc364558a3ef8a7e1d
colinxu2020 <colinxu2020@gmail.com>
2025-08-24 14:58:41 +0800

    :white_check_mark: Fix Tests

--- tests/test_b.py
+++ tests/test_b.py
@@ -5,7 +5,7 @@ from pytest import raises
 from slhdsa import KeyPair, shake_256s
 from slhdsa.lowlevel.fors import FORS
 from slhdsa.lowlevel.xmss import XMSS
-from slhdsa.lowlevel.addresses import Address
+from slhdsa.lowlevel.addresses import FORSTreeAddress, Address
 import slhdsa.lowlevel.parameters
 
 
@@ -15,12 +15,12 @@ def test1():
     assert str(kp.pub) == f'<SLHDSA Public Key: {kp.pub.digest().hex()}>'
     
 def test2():
-    assert FORS(shake_256s).node(b"", 0, shake_256s.a+1, b"", Address(0, 0)) == b""
+    assert FORS(shake_256s).node(b"", 0, shake_256s.a+1, b"", FORSTreeAddress(0, 0)) == b""
     assert XMSS(shake_256s).node(b"", 0, shake_256s.h_m+1, b"", Address(0, 0)) == b""
     
 def test3():
     with raises(ValueError):
-        slhdsa.lowlevel.parameters.mgf1_sha256(b"", sha256().digest_size << 32)
+        slhdsa.lowlevel.parameters.mgf1_sha256(b"", (sha256().digest_size << 32) + 1)
     with raises(ValueError):
-        slhdsa.lowlevel.parameters.mgf1_sha512(b"", sha512().digest_size << 32)
+        slhdsa.lowlevel.parameters.mgf1_sha512(b"", (sha512().digest_size << 32) + 1)
         
