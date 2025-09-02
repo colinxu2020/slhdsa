2cc8eaa24ad41b9daef2607b0ddd8f8055f25b9d
colinxu2020 <colinxu2020@gmail.com>
2025-09-02 12:27:09 +0800

    :fire: Remove Useless File

--- build_mypyc.py
+++
@@ -1,9 +0,0 @@
-from os import system
-from pathlib import Path
-
-
-sources = []
-for pth in Path('slhdsa').glob('**/*.py'):
-    if pth.name!='__init__.py':
-        sources.append(pth.as_posix().replace('/', '\\'))
-system(' '.join(['mypyc']+sources))
