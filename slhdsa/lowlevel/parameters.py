cc92d777c1d5dcf63f943a6a6fb3ca945a61c50e
colinxu2020 <colinxu2020@gmail.com>
2025-10-08 17:17:44 +0800

    :bug: Fix Pronunciation Error

--- slhdsa/lowlevel/parameters.py
+++ slhdsa/lowlevel/parameters.py
@@ -26,7 +26,7 @@ class Parameter:
     Tl: Callable[[bytes, Address, bytes], bytes]
     
     @property
-    def openid(self):
+    def objectid(self):
         return (2, 16, 840, 1, 101, 3, 4, 3, self.oid_no)
 
 
